e4765c3a8cd60bd5462432952fb4c609d3fe6e56
kumbhani bhavesh <kumbhani.bhavesh.1@gmail.com>
2018-12-12 17:07:00 +0530

    update for chart

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -13,6 +13,9 @@ export class DashboardComponent implements OnInit {
   canvas: any;
   ctx: any;
 
+  canvas1:any;
+  ctx1:any;
+
   revenue:any;
   today:any;
 
@@ -20,72 +23,82 @@ export class DashboardComponent implements OnInit {
     this.canvas = document.getElementById('myChart');
     this.ctx = this.canvas.getContext('2d');
     let myChart = new Chart(this.ctx, {
-      type: 'line',
+      type: 'bar',
       data: {
-				labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-				datasets: [{
-					label: 'Unfilled',
-					fill: true,
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+              {
+                  label: '',
+                  backgroundColor: '#920909',
+                  borderColor: '#1E88E5',
+                  data: [63, 58, 82, 81, 56, 55, 41]
+              },
+              {
+                  label: '',
+                  backgroundColor: '#04033b',
+                  borderColor: '#7CB342',
+                  data: [27, 49, 45, 19, 87, 28, 90]
+              },
+              {
+                label: '',
+                backgroundColor: '#0c6790',
+                borderColor: '#7CB342',
+                data: [2, 50, 40, 29, 80, 27, 40]
+              },
 
-          backgroundColor: 'rgba(255, 206, 86, 1)',
-					borderColor: 'rgba(255, 206, 86, 1)',
-          borderDash: [5, 5],
-          data: [
-						2,4,5,7,8,0,5
-					],
-				}, {
-					label: 'Dashed',
-          fill: true,
+          ]
+      },
+      options: {
+        responsive: false,
+        display:true,
+        scales: {
+          yAxes:[{
+            ticks:{
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+    });
 
-					backgroundColor: 'rgba(255, 99, 132, 1)',
-					borderColor: 'rgba(255, 99, 132, 1)',
-					borderDash: [5, 5],
-					data: [
-						7,9,10,12,13,5,10
-					],
-				}, {
-					label: 'Filled',
-          fill: true,
-
-					backgroundColor: 'rgba(54, 162, 235, 1)',
-					borderColor: 'rgba(54, 162, 235, 1)',
-          borderDash: [5, 5],
-          data: [
-						10,12,13,15,17,8,15
-					],
-				}]
-			},
-			options: {
-				responsive: true,
-				title: {
-					display: false,
-					text: 'Chart.js Line Chart'
-				},
-				tooltips: {
-					mode: 'index',
-					intersect: false,
-				},
-				hover: {
-					mode: 'nearest',
-					intersect: true
-				},
-				scales: {
-					xAxes: [{
-						display: true,
-						scaleLabel: {
-							display: true,
-							labelString: 'Month'
-						}
-					}],
-					yAxes: [{
-						display: true,
-						scaleLabel: {
-							display: true,
-							labelString: 'Value'
-						}
-					}]
-				}
-			}
+    this.canvas1 = document.getElementById('myChart1');
+    this.ctx1 = this.canvas1.getContext('2d');
+    let myChart1 = new Chart(this.ctx1, {
+      type: 'bar',
+      data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+              {
+                  label: '',
+                  backgroundColor: '#920909',
+                  borderColor: '#1E88E5',
+                  data: [63, 58, 82, 81, 56, 55, 41]
+              },
+              {
+                  label: '',
+                  backgroundColor: '#04033b',
+                  borderColor: '#7CB342',
+                  data: [27, 49, 45, 19, 87, 28, 90]
+              },
+              {
+                label: '',
+                backgroundColor: '#0c6790',
+                borderColor: '#7CB342',
+                data: [2, 50, 40, 29, 80, 27, 40]
+              },
+          ]
+      },
+      options: {
+        responsive: false,
+        display:true,
+        scales: {
+          yAxes:[{
+            ticks:{
+              beginAtZero:true
+            }
+          }]
+        }
+      }
     });
 
 
